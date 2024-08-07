dc547930fbb1350eaf6bde84653b9ac973a411db
Jani Nikula <jani.nikula@intel.com>
2024-08-07 11:05:25 +0300

    doc: add redirects for moved pages
    When moving pages around I, for whatever reason, didn't consider that
    this creates a bunch of stale links. The drm-misc link in MAINTAINERS
    being perhaps the worst one to go 404.
    
    We could, and should, fix a lot of the now stale links, but in the mean
    time, use the sphinx-reredirects extension [1] to create redirects for
    the relocated pages.
    
    [1] https://documatt.com/sphinx-reredirects/
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- conf.py
+++ conf.py
@@ -14,7 +14,10 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.graphviz']
+extensions = [
+    'sphinx.ext.graphviz',
+    'sphinx_reredirects',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst', '.*']
@@ -36,4 +39,24 @@ html_theme_options = {
     'extra_nav_links': {
         'Project Home Page': 'https://gitlab.freedesktop.org/drm/maintainer-tools',
     }
+}
+
+# -- Options for sphinx-reredirects ------------------------------------------
+# https://documatt.com/sphinx-reredirects/usage.html
+
+redirects = {
+    'commit-access': 'committer/commit-access.html',
+    'committer-drm-intel': 'committer/committer-drm-intel.html',
+    'committer-drm-misc': 'committer/committer-drm-misc.html',
+    'committer-guidelines': 'committer/index.html',
+    'drm-intel': 'repositories/drm-intel.html',
+    'drm-misc': 'repositories/drm-misc.html',
+    'drm-tip': 'repositories/drm-tip.html',
+    'drm-xe': 'repositories/drm-xe.html',
+    'drm': 'repositories/drm.html',
+    'getting-started': 'committer/getting-started.html',
+    'maintainer-drm-intel': 'maintainer/maintainer-drm-intel.html',
+    'maintainer-drm-misc': 'maintainer/maintainer-drm-misc.html',
+    'maintainer-drm-xe': 'maintainer/maintainer-drm-xe.html',
+    'maintainer-guidelines': 'maintainer/index.html',
 }
